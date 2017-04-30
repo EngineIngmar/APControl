79c34b8557cf3d28e1afce0e2acbed7f4961bf89
engineIngmar <red-nova@web.de>
2017-04-30 20:51:49 +0200

    hotfix tested

--- sensors/DHT22.py
+++ sensors/DHT22.py
@@ -54,21 +54,24 @@ def logSensorValue(dbName,id,temp,hum):
         cursor = db.cursor()
         #id    TIMESTAMP    STATE_actual    STATE_target    TEMP_C    HUM_per    LEVEL_cm    FLOW_lmin
         cursor.execute('SELECT TIMESTAMP,TEMP_C FROM Log_system WHERE ID=? AND TEMP_C IS NOT NULL ORDER BY TIMESTAMP DESC LIMIT 1',[id])
-        """Check whether there is already a value in the database. If not, lastValue set to zero."""
         lastTemp = cursor.fetchone()[1]
+        
         cursor.execute('SELECT TIMESTAMP,HUM_per FROM Log_system WHERE ID=? AND HUM_per IS NOT NULL ORDER BY TIMESTAMP DESC LIMIT 1',[id])
-        """Check whether there is already a value in the database. If not, lastValue set to zero."""
         lastHum = cursor.fetchone()[1]
         
+        """Check whether there is already a value in the database. If not, lastValue set to zero."""
         if lastTemp is None:
             lastTemp = 0
-        else:
-            lastTemp = lastTemp
+        if lastHum is None:
+            lastHum = 0
+        print(hum)
         print(temp)
-        if (temp != lastTemp) or (hum != lastHum):
+        print(abs(lastTemp - temp))
+        if (hum != lastHum):
             if (0 <= hum <= 100):
                 cursor.execute("INSERT INTO Log_system(ID,HUM_per) VALUES(?,?)",(id,hum))
                 db.commit()
+        if (temp != lastTemp):
             if (abs(lastTemp - temp) <= 5):
                 cursor.execute("INSERT INTO Log_system(ID,TEMP_C) VALUES(?,?)",(id,temp))
                 db.commit()
@@ -86,7 +89,7 @@ def getSensorValue(dbName,id):
         cursor.execute("SELECT TIMESTAMP,TEMP_C FROM Log_system WHERE ID=? AND TEMP_C IS NOT NULL ORDER BY TIMESTAMP DESC LIMIT 1",[id]);
         temp = cursor.fetchone()[1]
         cursor.execute("SELECT TIMESTAMP,HUM_per FROM Log_system WHERE ID=? AND HUM_per IS NOT NULL ORDER BY TIMESTAMP DESC LIMIT 1",[id]);
-        hum = cursor.fetchone()[2]
+        hum = cursor.fetchone()[1]
         sensorValue = [temp,hum]
     except Exception as e:
         cursor.execute("INSERT INTO Log_software(MODULE,MESSAGE) VALUES('DHT22.py getSensorValue',?)",[repr(e)])
